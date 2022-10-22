e99b9c74ab8e7af45308331b75189cacc796906f
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-10-22 11:57:45 +0500

    my first telegram bot

---
+++ main.py
@@ -0,0 +1,99 @@
+import requests
+from pprint import pprint
+from Token import TOKEN
+
+
+class Messages:
+    def __init__(self, TOKEN):
+        self.TOKEN = TOKEN
+
+    def get_updates(self):
+        r = requests.get(
+            f'https://api.telegram.org/bot{self.TOKEN}/getUpdates')
+        updates = r.json()
+        return updates
+
+    def send_message(self, chat_id):
+        url = f'https://api.telegram.org/bot{self.TOKEN}/sendMessage'
+        data = {
+            'chat_id': chat_id,
+            'text': "Please write cat,dog or contact!!"
+        }
+        requests.post(url, data=data)
+
+    def send_photo(self, text, chat_id):
+        url = f'https://api.telegram.org/bot{self.TOKEN}/sendPhoto'
+        if text == 'cat':
+            r = requests.get('https://aws.random.cat/meow')
+            data = r.json()
+            img_url = data.get('file')
+            data = {
+                'chat_id': chat_id,
+                'photo': img_url,
+                'caption': f"<i><b>#CAT</b></i>",
+                'parse_mode': 'HTML'
+            }
+            requests.post(url, data=data)
+        else:
+            r = requests.get('https://dog.ceo/api/breeds/image/random')
+            data = r.json()
+            img_url = data.get('message')
+            data = {
+                'chat_id': chat_id,
+                'photo': img_url,
+                'caption': f"<i><b>#DOG</b></i>",
+                'parse_mode': 'HTML'
+            }
+            requests.post(url, data=data)
+
+    def send_contact(self, chat_id, text):
+        url = f'https://api.telegram.org/bot{TOKEN}/sendContact'
+        if text == "contact_1":
+            data = {
+                'chat_id': chat_id,
+                'phone_number': '+998997444358',
+                'first_name': "Diyorbek",
+                'last_name': 'Fafandiro'
+            }
+        elif text == "contact_2":
+            data = {
+                'chat_id': chat_id,
+                'phone_number': '+998943577744',
+                'first_name': "Javohir",
+                'last_name': 'Jalilov'
+            }
+        else:
+            data = {
+                'chat_id': chat_id,
+                'phone_number': '+998973935107',
+                'first_name': "Akbar",
+                'last_name': 'Abdullayev'
+            }
+
+        r = requests.post(url, data=data)
+
+
+def read_txt(last_message, chat_id):
+    if 'contact' in last_message:
+        f.send_contact(chat_id=chat_id, text=last_message)
+    elif last_message.lower() in ('cat', "dog"):
+        f.send_photo(text=last_message.lower(), chat_id=chat_id)
+    else:
+        f.send_message(chat_id=chat_id)
+
+
+f = Messages(TOKEN)
+old_last_message_id = -1
+while True:
+    data = f.get_updates()
+
+    message_data = data['result'][-1]['message']
+
+    last_message_id = message_data['message_id']
+
+    chat_id = message_data['chat']['id']
+
+    if last_message_id != old_last_message_id:
+
+        old_last_message_id = last_message_id
+        read_txt(message_data['text'], chat_id)
